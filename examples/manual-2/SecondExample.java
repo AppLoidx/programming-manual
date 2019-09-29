514ffa50d4e08fc98a8a2ac0f78afe7712e75767
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-09-29 16:10:37 +0300

    add book cover and examples

--- examples/manual-2/SecondExample.java
+++ examples/manual-2/SecondExample.java
@@ -1,7 +1,41 @@
-package PACKAGE_NAME;
 
-/**
- * @author Arthur Kupriyanov
- */
+
 public class SecondExample {
+    static class Grig {
+        double length;
+        String color;
+        String name;
+
+        Grig(String grigsName, String grigsColor, double grigsLength){
+            name = grigsName;
+            color = grigsColor;
+            length = grigsLength;
+        }
+
+        void jump(){
+            System.out.println(name + " is jumping");
+        }
+
+        void eat(){
+            System.out.println(name + " is eating grass");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // создаем объект-кузнечик Bob
+        Grig bob = new Grig("Bob", "brown", 5d);
+
+        // создаем объект-кузнечик Alice
+        Grig alice = new Grig("Alice", "green", 4.9d);
+
+
+        bob.jump();     // Bob is jumping
+        alice.jump();   // Alice is jumping
+        bob.eat();      // Bob is eating grass
+        alice.eat();    // Alice is eating grass
+
+    }
 }
